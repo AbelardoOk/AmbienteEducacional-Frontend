c78e9f223a861eec688d18a1d95992a1eeef06c1
AbelardoOk <abelardoemailprivado@gmail.com>
2025-03-23 11:06:26 -0400

    Fix: Correção de requisição ao registro

--- src/api/auth/register.ts
+++ src/api/auth/register.ts
@@ -1,5 +1,7 @@
 export const register = async (name: string, password: string) => {
   const type = "aluno";
+  const turmaId = 1;
+  console.log({ type, name, password, turmaId });
 
   try {
     const response = await fetch(
@@ -9,16 +11,19 @@ export const register = async (name: string, password: string) => {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ type, name, password }),
+        body: JSON.stringify({ type, name, password, turmaId }),
       },
     );
 
     if (!response.ok) {
+      const errorDetails = await response.text(); // Pega o conteúdo da resposta de erro
+      console.error("Erro ao registrar o usuário:", errorDetails);
       throw new Error("Usuário ou senha inválidos");
     }
     const data = await response.json();
     return data; // Retorna o resultado (como o token)
   } catch (error) {
-    throw new Error(error.message || "Erro na requisição de login");
+    console.error("Erro na requisição de registro:", error);
+    throw new Error(error.message || "Erro na requisição de registro");
   }
 };
